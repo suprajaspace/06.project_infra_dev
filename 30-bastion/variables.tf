4c92b7b37e1f325a97fef6abac3710117ac03f7a
Supraja <supra556699@gmail.com>
2024-12-05 21:50:44 +0800

    project infrastructure modules: ec2-sg-vpc

--- 30-bastion/variables.tf
+++ 30-bastion/variables.tf
@@ -14,25 +14,7 @@ variable "common_tags" {
     }
 }
 
-variable "mysql_sg_tags" {
-    default = {
-        Component = "mysql"
-    }
-}
-
-variable "backend_sg_tags" {
-    default = {
-        Component = "backend"
-    }
-}
-
-variable "frontend_sg_tags" {
-    default = {
-        Component = "frontend"
-    }
-}
-
-variable "bastion_sg_tags" {
+variable "bastion_tags" {
     default = {
         Component = "bastion"
     }
